af4ac7dc714183e02897b34ae27c0490135d993f
gsimsekfb <gokhansimsekfb@gmail.com>
2020-04-10 16:48:43 +0300

    First working list

--- App.js
+++ App.js
@@ -1,114 +1,120 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, SectionList } from 'react-native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+export default class App extends Component {
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: DATA
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <SectionList
+          sections={this.state.data}
+          renderSectionHeader={({section}) => {
+            return (
+              <View style={styles.titleContainer}>
+                <Text style={styles.title}>
+                  {section.title}
+                </Text>
+              </View>
+            )
+          }}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity style={styles.itemContainer} onPress={() => {}}>
+                <Image style={styles.itemImage} source={{uri: item.image}}/>
+                <Text style={styles.itemText}>{item.key}</Text>
+              </TouchableOpacity>
+            )
+          }}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  root:{
+    marginTop:20,
+    padding:10,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  container: {
+    paddingVertical: 12,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+  },  
+  titleContainer:{
+    shadowColor: '#00000021',
+    shadowOffset: {
+      width: 2
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    marginVertical: 8,
+    backgroundColor:"#DCDCDC",
+    padding:10
   },
-  body: {
-    backgroundColor: Colors.white,
+  title:{
+    fontSize:25,
+    color:"#000000"
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  itemContainer: {    
+    height:90,
+    margin: 4,
+    paddingVertical: 4,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    // backgroundColor: 'yellow'
+  },    
+  itemImage:{
+    width:80,
+    height:80,
+    borderRadius:4,
+    marginLeft:20,
+  },  
+  itemText: {
+    textAlignVertical: 'center',
+    height:80,
+    marginLeft: 16,    
+    fontSize:16,
+    fontWeight:"bold",
+    // backgroundColor: 'red',
   },
 });
-
-export default App;
+ 
+const DATA = [
+  {
+    title:"A", 
+    data:[
+      {key:'Adana', image:"https://i.pinimg.com/originals/f5/87/a8/f587a8486a11272b8d9854d7830e95bf.jpg"},
+      {key:'Ankara', image:"https://lezzetler.com/images/yuklenen4/beypazari-guveci-145600.jpg"},
+      {key:'Aydin', image:"https://cdn.yemek.com/uploads/2016/05/keskek-aydin.jpg"},
+    ]
+  },
+  {
+    title:"B", 
+    data:[
+      {key:'User 1', image:"https://bootdey.com/img/Content/avatar/avatar3.png"},
+      {key:'User 2', image:"https://bootdey.com/img/Content/avatar/avatar4.png"},
+    ]
+  },
+  {
+    title:"C", 
+    data:[
+      {key:'User 1', image:"https://bootdey.com/img/Content/avatar/avatar5.png"},
+    ]
+  },
+  {
+    title:"D", 
+    data:[
+      {key:'User 1', image:"https://bootdey.com/img/Content/avatar/avatar2.png"},
+    ]
+  },
+];
